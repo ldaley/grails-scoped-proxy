43a496f9b8ee454059241fb9277b5d2e5bf30db6
Luke Daley <ld@ldaley.com>
2010-06-03 16:46:40 +1000

    Added datasource and doc config.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -2,3 +2,37 @@ log4j = {
     error ''
     debug 'grails.plugin.scopedproxy'
 }
+
+dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
+}
+
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop"
+            url = "jdbc:hsqldb:mem:devDB"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "create-drop"
+            url = "jdbc:hsqldb:mem:testDb"
+        }
+    }
+}
+
+grails {
+    doc {
+        authors = "Luke Daley"
+        license = "Apache License 2.0"
+    }
+}
