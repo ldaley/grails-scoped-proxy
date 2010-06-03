23f1d99f2a0bc049e7381f3f03c8bf336b076d41
Luke Daley <ld@ldaley.com>
2010-06-03 17:34:20 +1000

    Remove data source stuff.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -3,33 +3,6 @@ log4j = {
     debug 'grails.plugin.scopedproxy'
 }
 
-dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
-}
-
-environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:hsqldb:mem:devDB"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
-}
-
 grails {
     doc {
         authors = "Luke Daley"
