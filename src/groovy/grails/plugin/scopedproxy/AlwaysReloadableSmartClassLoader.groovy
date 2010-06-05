c12554ff3d300f9546caea9a2a0726bdf8b21f71
Luke Daley <ld@ldaley.com>
2010-06-05 14:47:32 +1000

    Added docs on why this is needed.

--- src/groovy/grails/plugin/scopedproxy/AlwaysReloadableSmartClassLoader.groovy
+++ src/groovy/grails/plugin/scopedproxy/AlwaysReloadableSmartClassLoader.groovy
@@ -2,6 +2,11 @@ package grails.plugin.scopedproxy
 
 import org.springframework.core.SmartClassLoader
 
+/**
+ * Spring AOP proxy generation looks for class loaders of this type to determine
+ * whether proxies for classes can be cached or not. This implementation says that
+ * every class is always reloadable so completely disables caching.
+ */
 class AlwaysReloadableSmartClassLoader extends ClassLoader implements SmartClassLoader {
 
 	AlwaysReloadableSmartClassLoader(ClassLoader parent) {
