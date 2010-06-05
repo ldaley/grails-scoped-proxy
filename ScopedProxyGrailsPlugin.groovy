d7fc8961941911a5a2e0a41f2e93ded4e6b298b8
Luke Daley <ld@ldaley.com>
2010-06-05 14:59:13 +1000

    Moved plugin class into ‘grails.plugin.scopedproxy’ package.

--- ScopedProxyGrailsPlugin.groovy
+++ ScopedProxyGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import grails.plugin.scopedproxy.*
+package grails.plugin.scopedproxy
 
 import grails.plugin.scopedproxy.reload.session.*
 import grails.plugin.scopedproxy.reload.ScopedBeanReloadListener
