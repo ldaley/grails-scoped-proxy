a26d5904cbb975d2f1092a25094d661f0a1716b1
Keith Zimmerman <keithrz@gmail.com>
2014-01-13 12:36:09 -0600

    Update Grails version to 2.3.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,12 +17,21 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.dependency.resolver = "maven"
+
 grails.project.dependency.resolution = {
     inherits( "global" ) {}
     log "warn"
     repositories {        
         grailsPlugins()
         grailsHome()
+		mavenLocal()
+		grailsCentral()
+		mavenCentral()
     }
     dependencies {}
+	plugins {
+		build ":tomcat:7.0.47"
+		runtime ":hibernate:3.6.10.6"
+	}
 }
